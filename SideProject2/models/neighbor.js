a8499282abbbbdd1272ca1d352ee15036d737a14
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-08-09 10:43:44 -0600

    deleted some previous comments

--- SideProject2/models/neighbor.js
+++ SideProject2/models/neighbor.js
@@ -1,12 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var Neighbor = sequelize.define("Neighbor", {
-    // Giving the Author model a name of type STRING
     name: DataTypes.STRING
   });
 
   Neighbor.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
     Neighbor.hasMany(models.Post, {
       onDelete: "cascade"
     });
